122632e560f0971d5d0fdc17dfd143542d994297
Jesse Ilescas <ilesmj9516@gmail.com>
2022-06-07 20:01:25 -0500

    Agregando contador

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import ItemCount from './ItemCount'
 
 function ItemListContainer(prop) {
-  return (
-    <div className='container'>
+    return (
+      <>
+      <div className='container'>
         <h1>{prop.promocion}</h1>
     </div>
+    <div className='container'>
+      <ItemCount/>
+    </div>
+      </>
   )
 }
 
